57d19e99b458a5b01784e98ec7475064d08ba658
cheretbe <a.a.orlov@gmail.com>
2019-06-17 22:06:02 +0200

    Add CIFS shares mounting wrapper script

---
+++ mounts.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description="SMB share mounting helper script")
+parser.add_argument("action", choices=["mount", "unmount"], help="Action to perform")
+parser.add_argument("mountpoint", help="path to mount (e.g. /smb/host/C)")
+parser.add_argument("--credentials", help="file that contains username, password and optionally domain")
+
+options = parser.parse_args()
+
+#print(sys.argv[1], flush=True)
+#subprocess.check_call("id")
+
+if options.action == "mount":
+    if not options.credentials:
+        print("--credentials option is required for mount")
+        sys.exit(1)
+
+    if os.path.ismount(options.mountpoint):
+        print("[!] WARNING: '{}' is already mounted. Dismounting".format(options.mountpoint))
+        subprocess.check_call(("/bin/umount", options.mountpoint))
+
+    os.makedirs(options.mountpoint, exist_ok=True)
+
+    host, drive_letter = options.mountpoint.rstrip("/").split("/")[-2:]
+    print("Mounting share 'backup_{}' on host '{}'".format(drive_letter, host), flush=True)
+
+    subprocess.check_call(("/bin/mount", "-t", "cifs",
+        "-o", "credentials={},uid=backuppc-client,gid=backuppc-client".format(options.credentials),
+        "//{}/backup_{}".format(host, drive_letter),
+        options.mountpoint
+    ))
+else:
+    print("Dismounting '{}'".format(options.mountpoint), flush=True)
+    subprocess.check_call(("/bin/umount", options.mountpoint))
