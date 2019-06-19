79d22d4bcf0897e82428afdf3eaf7bb099a8a614
cheretbe <a.a.orlov@gmail.com>
2019-06-19 22:44:41 +0200

    Add SMB protocol version option

--- mounts.py
+++ mounts.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(description="SMB share mounting helper script")
 parser.add_argument("action", choices=["mount", "unmount"], help="Action to perform")
 parser.add_argument("mountpoint", help="path to mount (e.g. /smb/host/C)")
 parser.add_argument("--credentials", help="file that contains username, password and optionally domain")
+parser.add_argument("--smb-version", help="SMB protocol version to use (e.g. 2.1)")
 
 options = parser.parse_args()
 
@@ -29,8 +30,12 @@ if options.action == "mount":
     host, drive_letter = options.mountpoint.rstrip("/").split("/")[-2:]
     print("Mounting share 'backup_{}' on host '{}'".format(drive_letter, host), flush=True)
 
+    mount_options = "credentials={},uid=backuppc-client,gid=backuppc-client".format(options.credentials)
+    if options.smb_version:
+        mount_options += ",vers={}".format(options.smb_version)
+
     subprocess.check_call(("/bin/mount", "-t", "cifs",
-        "-o", "credentials={},uid=backuppc-client,gid=backuppc-client".format(options.credentials),
+        "-o", mount_options,
         "//{}/backup_{}".format(host, drive_letter),
         options.mountpoint
     ))
