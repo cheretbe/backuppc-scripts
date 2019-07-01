24d3fd7560d1d439aca431f42c8dff70d852022d
cheretbe <a.a.orlov@gmail.com>
2019-07-01 15:08:06 +0200

    Rename wrapper script

--- delete_snapshot.bat
+++ delete_snapshot.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 IF "%SSHSESSIONID%" NEQ "" CHCP 65001
-powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass . '%~dp0run_remote_script.ps1' -scriptName 'DeleteSnapshot' %*
+powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass . '%~dp0run_snaphot_operation.ps1' -scriptName 'DeleteSnapshot' %*
 ECHO ERRORLEVEL: %ERRORLEVEL%
 EXIT /B
