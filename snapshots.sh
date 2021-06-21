97a185422494e959ead5d20299cf8666faed6016
cheretbe <a.a.orlov@gmail.com>
2021-06-21 20:38:01 +0200

    Switch to venv creation with online hosted script

--- snapshots.sh
+++ snapshots.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 script_dir="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-/usr/bin/python3 ${script_dir}/tools/setup_venv.py backuppc-scripts \
-  --requirement ${script_dir}/requirements.txt --batch-mode
+curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/master/setup_venv.py?flush_cache=True \
+  | /usr/bin/python3 - backuppc-scripts --requirement ${script_dir}/requirements.txt --batch-mode
 
 "${HOME}/.cache/venv/backuppc-scripts/bin/python3" \
   ${script_dir}/snapshots.py "$@"
