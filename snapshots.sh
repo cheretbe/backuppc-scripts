b41c39a462fc82fc0bce1824835725f986c4ddd2
cheretbe <a.a.orlov@gmail.com>
2021-06-21 21:49:38 +0200

    Remove unnecessary line break

--- snapshots.sh
+++ snapshots.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 script_dir="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-/usr/bin/curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/master/setup_venv.py?flush_cache=True \
-  | /usr/bin/python3 - backuppc-scripts --requirement ${script_dir}/requirements.txt --batch-mode
+if [ ! -d ${HOME}/.cache/venv/backuppc-scripts ]; then
+  /usr/bin/curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/master/setup_venv.py?flush_cache=True \
+    | /usr/bin/python3 - backuppc-scripts --requirement ${script_dir}/requirements.txt --batch-mode
+fi
 
-"${HOME}/.cache/venv/backuppc-scripts/bin/python3" \
-  ${script_dir}/snapshots.py "$@"
+"${HOME}/.cache/venv/backuppc-scripts/bin/python3" ${script_dir}/snapshots.py "$@"
